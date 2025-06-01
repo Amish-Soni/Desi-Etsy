b1c7fcf428964f7dc7670de40e79d5e456fcf2c2
Amish Soni <soni2003amish@gmail.com>
2025-06-01 15:35:33 +0530

    Boilerplate Code

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Init express
+const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Desi Etsy API is running...");
+});
+
+// Start server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h3>Infotact Project 1 Desi Etsy</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
