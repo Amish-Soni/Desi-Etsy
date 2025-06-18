2abd0faacff0504ae36c353cd52430a6a9192560
Preksha262004 <prekshathummar@gmail.com>
2025-06-18 17:32:46 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // This is the important part
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
