d26e8facf9b93800a6cdeb62e395ebada5a77756
Amish Soni <soni2003amish@gmail.com>
2025-06-25 20:21:28 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -106,7 +106,7 @@ export const forgotPasswordController = async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      port: parseInt(process.env.EMAIL_PORT),
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
