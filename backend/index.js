f0ab50da8c1f76798a79ef50de4afe11a20af335
Amish Soni <soni2003amish@gmail.com>
2025-07-13 12:34:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    origin: ["http://localhost:5173", "https://desietsy.netlify.app"],
     credentials: true,
   })
 );
